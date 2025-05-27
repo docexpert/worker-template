03f58262e4060be1220b593fea714c142df75715
docexpert <hidocexpert@gmail.com>
2025-05-27 18:12:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    const filename = url.searchParams.get("filename");
+    if (!filename) {
+      return new Response("Missing ?filename=", { status: 400 });
+    }
+
+    const client = new S3Client({
+      region: "auto",
+      endpoint: "https://72916a6493777ed93342623a8ac89a09.r2.cloudflarestorage.com",
+      credentials: {
+        accessKeyId: env.R2_ACCESS_KEY_ID,
+        secretAccessKey: env.R2_SECRET_ACCESS_KEY
+      }
+    });
+
+    const command = new PutObjectCommand({
+      Bucket: "docexpert-docs",
+      Key: filename,
+      ContentType: "application/pdf"
+    });
+
+    const signedUrl = await getSignedUrl(client, command, { expiresIn: 900 });
+    return Response.json({ url: signedUrl });
+  }
+};
