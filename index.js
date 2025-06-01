a903930406ba47b8c44d70e8e068190240c25cd7
docexpert <hidocexpert@gmail.com>
2025-05-31 20:09:14 -0400

    Update index.js
    Allow upload

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const filename = url.searchParams.get("filename");
+    const action = url.searchParams.get("action") || "upload"; // default to upload
 
     if (!filename) {
       return new Response("Missing ?filename=", { status: 400 });
@@ -15,20 +20,28 @@ export default {
       endpoint: "https://72916a6493777ed93342623a8ac89a09.r2.cloudflarestorage.com",
       credentials: {
         accessKeyId: env.R2_ACCESS_KEY_ID,
-        secretAccessKey: env.R2_SECRET_ACCESS_KEY,
+        secretAccessKey: env.R2_SECRET_ACCESS_KEY
       }
     });
 
-    const command = new PutObjectCommand({
-      Bucket: "docexpert-docs", // Update to your actual R2 bucket name
-      Key: filename,
-      ContentType: "application/pdf",
-    });
+    let command;
+    if (action === "view") {
+      command = new GetObjectCommand({
+        Bucket: "docexpert-docs",
+        Key: filename
+      });
+    } else {
+      command = new PutObjectCommand({
+        Bucket: "docexpert-docs",
+        Key: filename,
+        ContentType: "application/pdf"
+      });
+    }
 
     const signedUrl = await getSignedUrl(client, command, { expiresIn: 900 });
 
     return new Response(JSON.stringify({ url: signedUrl }), {
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json" }
     });
   }
 };
